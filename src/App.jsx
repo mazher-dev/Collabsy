3d1252a1e947d395dbc7adc31b744876b0939f1b
MH Code <mazheriftikhar1@gmail.com>
2024-10-19 15:24:12 +0500

    19-oct changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar + Footer/Navbar/Navbar'
 import HeroSection from './Components/HeroSection/HeroSection'
+import Titlesection from './Components/titlesection/titlesection'
+import Features from './Components/Features/Features'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
       <HeroSection />
+      <Features />
     </div>
   )
 }
---
+++ src/Components/title/title.jsx
@@ -0,0 +1,13 @@
+// RectangleButton.js
+import React from 'react';
+import "./titlesection.css";
+
+const title = ({ text }) => {
+  return (
+    <div className='button'>
+      {text}
+    </div>
+  );
+};
+
+export default title;
