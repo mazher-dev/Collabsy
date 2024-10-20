dd20a7ad5742328dcfe5696fc4e3d45748c81685
MH Code <mazheriftikhar1@gmail.com>
2024-10-20 23:36:39 +0500

    20-oct changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar + Footer/Navbar/Navbar'
 import HeroSection from './Components/HeroSection/HeroSection'
 import Titlesection from './Components/titlesection/titlesection'
 import Features from './Components/Features/Features'
+import Pricing from './Components/Pricing/Pricing'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Navbar />
       <HeroSection />
       <Features />
-      
+      <Pricing />
     </div>
   )
 }
--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import Titlesection from '../titlesection/titlesection'
 import "./Features.css"
 import Textsection from '../textsection/textsection'
+import graph from '../../assets/graph.svg';
+import tracking from '../../assets/tracking.svg';
+
 
 const Features = () => {
   return (
     <div className='Features'>
       <Titlesection text="Features" backgroundColor="#5e2bff" textColor="#fff"/>
+      
+
       <Textsection 
         title="Unlock Your Potential with Our" 
         subtitle="Admin Dashboard Features" 
       />
-    
+      <div className='twosection'>
+        <div className='left-section'>
+            <img src={graph} alt="" />
+            <h1>Real-Time Data Charts</h1>
+            <p>Visualize project metrics at a glance and make informed decisions quickly.</p>
+        </div>
+        <div className='right-section'>
+            <img src={tracking} alt="" />
+            <h1>Project Tracking</h1>
+            <p>Monitor progress and deadlines to keep your team on track.</p>
+        </div>
+      </div>
     </div>
   )
 }
